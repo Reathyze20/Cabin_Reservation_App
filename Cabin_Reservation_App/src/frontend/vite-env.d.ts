c1493b46ab35fbf5f7a949b19b3f88a5f73d119a
Tomáš Rambousek <reathyze20@gmail.com>
2026-02-12 15:11:18 +0100

    Remove duplicate nested folder

--- Cabin_Reservation_App/src/frontend/vite-env.d.ts
+++
@@ -1,9 +0,0 @@
-/// <reference types="vite/client" />
-/// <reference types="vite-plugin-pwa/client" />
-
-declare module "flatpickr/dist/l10n/cs.js" {
-  import { CustomLocale } from "flatpickr/dist/types/locale";
-  export const Czech: CustomLocale;
-  const cs: Record<string, CustomLocale>;
-  export default cs;
-}
